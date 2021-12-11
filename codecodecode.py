e31961a3053205927d09ef764f8d31e1c7ae2835
Nadia <90277588+karasenk@users.noreply.github.com>
2021-12-11 16:59:45 +0300

    Add files via upload

--- codecodecode.py
+++ codecodecode.py
@@ -1,75 +1,84 @@
-<<<<<<< HEAD
 import pygame
 
-if __name__ == '__main__':
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for i in range(self.height):
+            for j in range(self.width):
+                rect = (self.left + j * self.cell_size, self.top + i *
+                        self.cell_size), (self.cell_size, self.cell_size)
+                pygame.draw.rect(screen, (255, 255, 255), rect, 1)
+
+    def get_cell(self, mouse_pos):
+        result = None
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.left + j * self.cell_size <= mouse_pos[0] and \
+                        self.left + (j + 1) * self.cell_size >= mouse_pos[0] and \
+                        self.top + i * self.cell_size <= mouse_pos[1] and \
+                        self.top + (i + 1) * self.cell_size >= mouse_pos[1]:
+                    result = i, j
+                    break
+        print(result)
+        return result
+
+    def on_click(self, cell):
+        pass
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
+
+
+class Life(Board):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def get_click(self, mouse_pos, event):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            if event.button == 1:
+                self.add(cell)
+            elif event.button == 3:
+                self.living()
+
+    def add(self, cell):
+        self.board[cell[0]][cell[1]] = 1
+        rect = self.left + cell[1] * self.cell_size, self.top + cell[0] * self.cell_size,\
+               self.cell_size, self.cell_size
+        pygame.draw.rect(screen, (50, 230, 5), rect)
+
+    def living(self);
+     pass
+    def next_move(self):
+        pass
+
+if '__main__' == __name__:
     pygame.init()
-    pygame.display.set_caption('к щелчку')
-    size = 501, 501
+    size = 400, 300
     screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
+    life = Life(12, 9)
     running = True
-    v = 50
-    pose = (size[0] // 2, size[1] // 2)
-    goal = False
     screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, (0, 255, 0), pose, 20)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONUP:
-                goal = event.pos
-            if goal:
-                if pose == goal:
-                    goal = False
-                else:
-                    diff_x = goal[0] - pose[0]
-                    diff_y = goal[1] - pose[1]
-                    if diff_x != 0:
-                        x = diff_x // abs(diff_x) * v * clock.tick() / 1000
-                    if diff_y != 0:
-                        y = diff_y // abs(diff_y) * v * clock.tick() / 1000
-                    if abs(diff_x) > abs(diff_y):
-                        y = 0
-                    elif abs(diff_x) < abs(diff_y):
-                        x = 0
-                    pose = pose[0] + x, pose[1] + y
-                    screen.fill((0, 0, 0))
-                    pygame.draw.circle(screen, (0, 255, 0), pose, 20)
-
+                life.get_click(event.pos, event)
+        life.render(screen)
         pygame.display.flip()
-    pygame.quit()
-=======
-import pygame
-
-if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('шарики')
-    v = 100
-    size = 500, 500
-    screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
-    running = True
-    draw = False
-    screen.fill((0, 0, 0))
-    coords = []
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONUP:
-                screen.fill((0, 0, 0))
-                draw = True
-                coords.append([event.pos, 1])
-                pygame.draw.circle(screen, (255, 200, 250), event.pos, 10)
-        if draw:
-            for c in coords:
-                if c[0] + 1 >= size[0]:
-                    coef = -1
-                pygame.draw.circle(screen, (255, 200, 250), pose, 10)
-
-        pygame.display.flip()
-    pygame.quit()
-
-#  сделой коеффициент (1 и -1) для направления
->>>>>>> origin/master
